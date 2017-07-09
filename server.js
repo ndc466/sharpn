dcb71c688265ce61c29fb59ebd5d57d9b4f5b84c
Nolan Corcoran <nolan@identity.utexas.edu>
2017-07-09 13:50:30 -0500

    test

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
+var http = require('http');
 var app = express();
 var path = require('path');
 var port = process.env.PORT || 8088;
 var bodyParser = require('body-parser');
-var morgan = require('morgan');
+//var morgan = require('morgan');
 var routes = require('./app/routes/api');
 
 /*
@@ -12,7 +13,7 @@ var router = express.Router(); // Invoke the Express Router
 var appRoutes = require('./app/routes/api')(router); // Import the application end points/API
 
 */
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public')); // Allow front end to access public folder
@@ -21,15 +22,20 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/', routes);
 
 var https_redirect = function(req, res, next) {
+	console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'production') {
-    	console.log(req.headers['x-forwarded-proto']);
-    	console.log(req.headers.protocol);
+    	//console.log(req.headers['x-forwarded-proto']);
+    	console.log('prod mode');
+    	console.log(req.protocol);
         if (req.headers['x-forwarded-proto'] != 'https') {
             return res.redirect('https://' + req.headers.host + req.url);
         } else {
             return next();
         }
     } else {
+    	console.log('dev mode');
+    	console.log(req.protocol);
+    	//console.log(process.env.NODE_ENV);
         return next();
     }
 };
